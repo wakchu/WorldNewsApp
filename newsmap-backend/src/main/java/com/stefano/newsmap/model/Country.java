8625418df17ee4d5363cf090e34499cd0373d9da
wakchu <stfnbrdn@gmail.com>
2025-10-25 15:43:50 +0200

    Update to country model

--- newsmap-backend/src/main/java/com/stefano/newsmap/model/Country.java
+++ newsmap-backend/src/main/java/com/stefano/newsmap/model/Country.java
@@ -1,9 +1,9 @@
 package com.stefano.newsmap.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "country")
@@ -13,20 +13,35 @@ public class Country {
     @Column(name = "iso_code", columnDefinition = "CHAR(2)", updatable = false)
     private String isoCode;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 150)
     private String name;
 
+    @Column(length = 100)
     private String region;
 
+    @Column(name = "lat", precision = 9, scale = 6)
+    private BigDecimal lat;
+
+    @Column(name = "lon", precision = 9, scale = 6)
+    private BigDecimal lon;
+
+    @Column(length = 50)
+    private String timezone;
+
+    @Column(name = "inserted_at", updatable = false, insertable = false)
+    private LocalDateTime insertedAt;
+
     // Costruttore di default richiesto da JPA
-    public Country() {
-    }
+    public Country() {}
 
     // Costruttore completo
-    public Country(String isoCode, String name, String region) {
+    public Country(String isoCode, String name, String region, BigDecimal lat, BigDecimal lon, String timezone) {
         this.isoCode = isoCode;
         this.name = name;
         this.region = region;
+        this.lat = lat;
+        this.lon = lon;
+        this.timezone = timezone;
     }
 
     // Getter e Setter
@@ -54,13 +69,44 @@ public class Country {
         this.region = region;
     }
 
-    // toString() utile per debug
+    public BigDecimal getLat() {
+        return lat;
+    }
+
+    public void setLat(BigDecimal lat) {
+        this.lat = lat;
+    }
+
+    public BigDecimal getLon() {
+        return lon;
+    }
+
+    public void setLon(BigDecimal lon) {
+        this.lon = lon;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+
+    public LocalDateTime getInsertedAt() {
+        return insertedAt;
+    }
+
     @Override
     public String toString() {
         return "Country{" +
                 "isoCode='" + isoCode + '\'' +
                 ", name='" + name + '\'' +
                 ", region='" + region + '\'' +
+                ", lat=" + lat +
+                ", lon=" + lon +
+                ", timezone='" + timezone + '\'' +
+                ", insertedAt=" + insertedAt +
                 '}';
     }
 }
